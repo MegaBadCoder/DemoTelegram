4cd8d57b71588b9fd338a61e41d9fec75001b865
root <root@1515503-cf91754.tw1.ru>
2023-05-22 19:16:47 +0300

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,16 @@ module.exports = {
         {
             name: 'Telegram Bot',
             script: 'src/index.ts',
-            interpreter: '/root/.nvm/versions/node/v18.16.0/bin/ts-node-dev',
-            args: '--transpile-only',
+            interpreter: '/root/.nvm/versions/node/v18.16.0/bin/ts-node',
+            //node_args: '-r/ tsconfig-paths/register',
+	    interpreter_args: '--project ./tsconfig.json',
+	    args: '--transpile-only',
         	    watch: true,
             env: {
-                NODE_ENV: 'production'
+                NODE_ENV: 'production',
+		//TS_NODE_PROJECT: './tsconfig.json'
             }
 
         }
     ]
 }
-
